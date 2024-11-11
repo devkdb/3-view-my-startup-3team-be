dbe972b1cf3783040f7da7e663684a0e27f8d469
Bluemoon105 <118960498+Bluemoon105@users.noreply.github.com>
2024-11-11 13:18:55 +0900

    edit

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,8 +5,8 @@ const prisma = new PrismaClient();
 
 async function main() {
   // 테이블을 비우고 시퀀스를 초기화
-  await prisma.$executeRaw`TRUNCATE TABLE "Selection" RESTART IDENTITY CASCADE`;
-  await prisma.$executeRaw`TRUNCATE TABLE "Comparison" RESTART IDENTITY CASCADE`;
+  // await prisma.$executeRaw`TRUNCATE TABLE "Selection" RESTART IDENTITY CASCADE`;
+  // await prisma.$executeRaw`TRUNCATE TABLE "Comparison" RESTART IDENTITY CASCADE`;
   await prisma.$executeRaw`TRUNCATE TABLE "MockInvestor" RESTART IDENTITY CASCADE`;
   await prisma.$executeRaw`TRUNCATE TABLE "Startup" RESTART IDENTITY CASCADE`;
   await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
@@ -29,24 +29,24 @@ async function main() {
     skipDuplicates: true,
   });
 
-  // Selection 데이터 삽입
-  await prisma.selection.createMany({
-    data: SELECTIONS,
-    skipDuplicates: true,
-  });
+  // // Selection 데이터 삽입
+  // await prisma.selection.createMany({
+  //   data: SELECTIONS,
+  //   skipDuplicates: true,
+  // });
 
-  // Comparison 데이터 삽입
-  await prisma.comparison.createMany({
-    data: COMPARISONS,
-    skipDuplicates: true,
-  });
+  // // Comparison 데이터 삽입
+  // await prisma.comparison.createMany({
+  //   data: COMPARISONS,
+  //   skipDuplicates: true,
+  // });
 
   // 각 테이블의 시퀀스를 재설정
   await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Category"', 'id'), (SELECT MAX(id) FROM "Category"))`;
   await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Startup"', 'id'), (SELECT MAX(id) FROM "Startup"))`;
   await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"MockInvestor"', 'id'), (SELECT MAX(id) FROM "MockInvestor"))`;
-  await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Selection"', 'id'), (SELECT MAX(id) FROM "Selection"))`;
-  await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Comparison"', 'id'), (SELECT MAX(id) FROM "Comparison"))`;
+  // await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Selection"', 'id'), (SELECT MAX(id) FROM "Selection"))`;
+  // await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Comparison"', 'id'), (SELECT MAX(id) FROM "Comparison"))`;
 }
 
 main()
