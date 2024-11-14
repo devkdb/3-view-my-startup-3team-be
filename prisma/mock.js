720f82ddc7ba39dbdfe90ae891856bf737fde73a
Bluemoon105 <118960498+Bluemoon105@users.noreply.github.com>
2024-11-14 18:47:40 +0900

    edit schema

--- prisma/mock.js
+++ prisma/mock.js
@@ -1041,30 +1041,30 @@ export const MOCK_INVESTORS = [
 //   },
 // ]
 		
-// export const COMPARISONS = [
-//   {
-//     id: 1,
-//     compare: true, 
-//     startupId: 6,
-//   },
-//   {
-//     id: 2,
-//     compare: true, 
-//     startupId: 7,
-//   },
-//   {
-//     id: 3,
-//     compare: true, 
-//     startupId: 8,
-//   },
-//   {
-//     id: 4,
-//     compare: true, 
-//     startupId: 9,
-//   },
-//   {
-//     id: 5,
-//     compare: true, 
-//     startupId: 10,
-//   },
-// ]	
+export const COMPARISONS = [
+  {
+    id: 1,
+    compare: true, 
+    startupId: 6,
+  },
+  {
+    id: 2,
+    compare: true, 
+    startupId: 7,
+  },
+  {
+    id: 3,
+    compare: true, 
+    startupId: 8,
+  },
+  {
+    id: 4,
+    compare: true, 
+    startupId: 9,
+  },
+  {
+    id: 5,
+    compare: true, 
+    startupId: 10,
+  },
+]	
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import { CATEGORIES, STARTUPS, MOCK_INVESTORS} from "./mock.js";
+import { CATEGORIES, STARTUPS, MOCK_INVESTORS, COMPARISONS} from "./mock.js";
 
 const prisma = new PrismaClient();
 
 async function main() {
   // 테이블을 비우고 시퀀스를 초기화
   // await prisma.$executeRaw`TRUNCATE TABLE "Selection" RESTART IDENTITY CASCADE`;
-  // await prisma.comparison.deleteMany();
+  await prisma.comparison.deleteMany();
   await prisma.mockInvestor.deleteMany();
   await prisma.startup.deleteMany();
   await prisma.category.deleteMany();
@@ -35,18 +35,18 @@ async function main() {
   //   skipDuplicates: true,
   // });
 
-  // // Comparison 데이터 삽입
-  // await prisma.comparison.createMany({
-  //   data: COMPARISONS,
-  //   skipDuplicates: true,
-  // });
+  // Comparison 데이터 삽입
+  await prisma.comparison.createMany({
+    data: COMPARISONS,
+    skipDuplicates: true,
+  });
 
   // 각 테이블의 시퀀스를 재설정
   await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Category"', 'id'), (SELECT MAX(id) FROM "Category"))`;
   await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Startup"', 'id'), (SELECT MAX(id) FROM "Startup"))`;
   await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"MockInvestor"', 'id'), (SELECT MAX(id) FROM "MockInvestor"))`;
   // await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Selection"', 'id'), (SELECT MAX(id) FROM "Selection"))`;
-  // await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Comparison"', 'id'), (SELECT MAX(id) FROM "Comparison"))`;
+  await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Comparison"', 'id'), (SELECT MAX(id) FROM "Comparison"))`;
 }
 
 main()
