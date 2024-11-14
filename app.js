30c67d18555a7286c57903ca8653f65f01fb0563
Bluemoon105 <118960498+Bluemoon105@users.noreply.github.com>
2024-11-14 14:21:11 +0900

    edit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { PrismaClient, Prisma } from '@prisma/client';
+import { number } from 'superstruct';
 // import { CreateUser, PatchUser } from './structs.js';
 // import { assert } from 'superstruct';
 
@@ -44,9 +45,9 @@ app.get('/startups', async (req, res) => {
       orderBy = { id: 'asc' };
       break;
   }
-
   try {
     const startups = await prisma.startup.findMany({
+      include:{category: true},
       orderBy,
       skip: parseInt(offset),
       take: parseInt(limit),
@@ -55,6 +56,63 @@ app.get('/startups', async (req, res) => {
     
   } catch (error) { res.status(500).send({ message: error.message }); }
 });
+
+//특정 기업 상세 조회
+app.get("/startups/:id", async (req, res) => {
+  const {id} = req.params;
+  const numId = parseInt(id, 10);
+  try {
+    const startup = await prisma.startup.findUnique({
+      where: { id: numId },
+    });
+    const serializedStartups = JSON.stringify(startup, replacer); res.send(serializedStartups);
+  }catch(error) {res.status(404).send({message: error.message}); }
+});
+
+//검색 기능
+app.get("/startups/search", async (req, res) => {
+  const { searchKeyword, offset = 0, limit = 10} =req.query;
+  console.log(searchKeyword);
+  try {
+    const startup = await prisma.startup.findMany({
+      orderBy: {id: "asc"},
+      skip: parseInt(offset),
+      take: parseInt(limit),
+      where:{
+          name: {contains: searchKeyword},
+      },
+    });
+    const serializedStartups = JSON.stringify(startup, replacer); res.send(serializedStartups);
+  }catch(error){res.status(404).send({message: error.message}); }
+})
+
+
+app.get('/selection', async (req, res) => {
+  const { offset = 0, limit = 10 } = req.query;
+  try{
+    const select = await prisma.startup.findMany({
+      select: {
+        name:true, 
+        count:true,
+      },
+      skip: parseInt(offset),
+      take: parseInt(limit),
+      orderBy: {id: "asc"},
+    });
+    res.status(200).send(select);
+  } catch(error) {res.status(400).send({message:error.message});}
+})
+
+app.post("/investments", async(req, res) => {
+  try{
+    const createInvest = await prisma.mockInvestor.create({
+      data: req.body
+    });
+    res.status(200).send(createInvest);
+  }catch(error) {res.status(400).send({message: error.message}); }
+})
+
+
 
 // 프론트랑 겹치니깐 8000으로 바꾼다.
 const port = process.env.PORT || 8000;
