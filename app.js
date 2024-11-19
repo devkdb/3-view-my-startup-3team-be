5c722aed6c5f5ce44eb12f9dfee1bead7bb97a1e
Kim Doobong <tengal73@naver.com>
2024-11-19 09:20:46 +0900

    # 서버 <--> 클라언트간의 CORS 문제 해결 -  npm install cors - local 연결은 http, 배포 서버 연결은 https
    # API 설정
    - app.js 파일에 모든 API 설정 정리
    - package.json, package-lock.json 파일에 관련 내용 적용.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import { PrismaClient, Prisma } from '@prisma/client';
-import { number } from 'superstruct';
+import express from "express";
+import cors from "cors";
+import { PrismaClient, Prisma } from "@prisma/client";
+import { number } from "superstruct";
 // import { CreateUser, PatchUser } from './structs.js';
 // import { assert } from 'superstruct';
 
@@ -10,6 +11,17 @@ const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+
+const corsOption = {
+  origin: [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "https://viewmystatup-db.onrender.com/startups/",
+  ],
+};
+app.use(cors(corsOption));
 
 // function asyncHandler(handler) {
 //   return async function (req, res) {
@@ -34,84 +46,109 @@ app.use(express.json());
 
 // BigInt 값을 문자열로 변환하여 JSON 응답 생성
 function replacer(key, value) {
-  return typeof value === 'bigint' ? value.toString() : value;
+  return typeof value === "bigint" ? value.toString() : value;
 }
 
-//전체 기업 조회
-app.get('/startups', async (req, res) => {
-  const { offset = 0, limit = 10, order = 'id' } = req.query;
+// 기업 전체 조회
+app.get("/companies", async (req, res) => {
+  const { offset = 0, limit = 10, order = "id" } = req.query;
   let orderBy;
   switch (order) {
-    case 'id':
-      orderBy = { id: 'asc' };
+    case "id":
+      orderBy = { id: "asc" };
       break;
   }
   try {
     const startups = await prisma.startup.findMany({
-      include:{category: true},
+      include: { category: true },
       orderBy,
       skip: parseInt(offset),
       take: parseInt(limit),
-      incluede:{category:true},
-    }); // BigInt 값을 문자열로 변환하여 JSON 응답 생성 
-    const serializedStartups = JSON.stringify(startups, replacer); res.send(serializedStartups);
-    
-  } catch (error) { res.status(500).send({ message: error.message }); }
+      incluede: { category: true },
+    }); // BigInt 값을 문자열로 변환하여 JSON 응답 생성
+    const serializedStartups = JSON.stringify(startups, replacer);
+    res.send(serializedStartups);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
+// 내 기업과 비교 대상 기벙들 비교하기 (정렬)
+// api: /companies/comparison GET
+// app.get("/companies/comparison", async (req, res) => {}
+
 //특정 기업 상세 조회
-app.get("/startups/:id", async (req, res) => {
-  const {id} = req.params;
+app.get("/companies/:id", async (req, res) => {
+  const { id } = req.params;
   const numId = parseInt(id, 10);
   try {
     const startup = await prisma.startup.findUnique({
       where: { id: numId },
     });
-    const serializedStartups = JSON.stringify(startup, replacer); res.send(serializedStartups);
-  }catch(error) {res.status(404).send({message: error.message}); }
+    const serializedStartups = JSON.stringify(startup, replacer);
+    res.send(serializedStartups);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
 });
 
-//검색 기능
-app.get("/startups/search", async (req, res) => {
-  const { searchKeyword, offset = 0, limit = 10} =req.query;
+// 내 기업의 순위와 근접한 순위의 기업 정보 확인
+// api: /companies/{companyId}/rank GET
+// app.get("/companies/id/rank", async (req, res) => {}
+
+// 검색 기능
+app.get("/companies/search", async (req, res) => {
+  const { searchKeyword, offset = 0, limit = 10 } = req.query;
   try {
     const startup = await prisma.startup.findMany({
-      orderBy: {id: "asc"},
+      orderBy: { id: "asc" },
       skip: parseInt(offset),
       take: parseInt(limit),
-      where:{
-          name: {contains: searchKeyword},
+      where: {
+        name: { contains: searchKeyword },
       },
     });
-    const serializedStartups = JSON.stringify(startup, replacer); res.send(serializedStartups);
-  }catch(error){res.status(404).send({message: error.message}); }
-})
+    const serializedStartups = JSON.stringify(startup, replacer);
+    res.send(serializedStartups);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+});
 
-
-//기업 선택 횟수 조회
-app.get('/selection', async (req, res) => {
+// 기업 선택 횟수 조회
+app.get("/selections", async (req, res) => {
   const { offset = 0, limit = 10 } = req.query;
-  try{
+  try {
     const select = await prisma.startup.findMany({
       select: {
-        name:true, 
-        count:true,
+        name: true,
+        count: true,
       },
       skip: parseInt(offset),
       take: parseInt(limit),
-      orderBy: {id: "asc"},
+      orderBy: { id: "asc" },
     });
     res.status(200).send(select);
-  } catch(error) {res.status(400).send({message:error.message});}
-})
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
 
-//전체 투자 현황 조회
+// 전체 투자 현황 조회
+// api: /investments GET
+// app.get("/investments", async (req, res) => {}
 
+// 특정기업에 투자하기
+// api: /investments POST
+// app.post("/investments", async (req, res) => {}
 
-//특정기업에 투자하기
+// 투자 수정
+// api: /investments/{investmentid} PUT
+// app.patch("/investments/:investmentid", async (req, res) => {}
 
-
-
+// 투자 삭제
+// api: /investments/{investmentid} DELETE
+// app.delete("/investments/:investmentid", async (req, res) => {}
 
 // 프론트랑 겹치니깐 8000으로 바꾼다.
 const port = process.env.PORT || 8000;
